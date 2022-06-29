d35122e397e125742e08dac81b130110e40c4c37
Ytallo Bruno <ytallo.bruno@calindra.com.br>
2022-06-29 15:09:52 -0300

    feat: botão criado

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+export default function Button(props) {
+    return (
+        <>
+            <button
+                className={styles.btn}
+                type={props.type}
+                onClick={props.click}
+            >
+                {props.text}{" "}
+            </button>
+        </>
+    );
+}
