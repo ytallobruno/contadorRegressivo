bd362ac633639e3f1dbca978d824466bff83a9eb
Ytallo Bruno <ytallo.bruno@calindra.com.br>
2022-06-29 14:37:36 -0300

    feat: criada pagina main

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Form from "../../components/Form/Form";
+
+export default function Main() {
+    return (
+        <div>
+            <Form />
+        </div>
+    );
+}
