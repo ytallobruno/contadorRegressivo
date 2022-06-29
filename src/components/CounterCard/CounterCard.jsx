06e56d7f5522b5d77d6a1a0843690e743c026680
Ytallo Bruno <ytallo.bruno@calindra.com.br>
2022-06-29 18:32:48 -0300

    feat: componente renomeado

--- src/components/CounterCard/CounterCard.jsx
+++ src/components/CounterCard/CounterCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import styles from "./ContadorCard.module.css";
+import styles from "./CounterCard.module.css";
 
-export default function ContadorCard({
+export default function CounterCard({
     titulo,
     timerDias,
     timerHoras,
